1d603e8ed421aec85ca345976fd954b0429282ca
yhoper <moisevic@hotmail.com>
2014-09-15 09:27:42 -0430

    Revert "TEST"
    This reverts commit 9d8df15982e415a7b252e772e9d2e0815732cf53.

--- app/views/profesionIndex.blade.php
+++ app/views/profesionIndex.blade.php
@@ -2,7 +2,7 @@
 @section('contenido')
           <div class="starter-template">
         <!--h1>Bootstrap starter template</h1>
-        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document. FINDING SHREDDER</p-->
+        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p-->
 
 
           <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." bla bla
